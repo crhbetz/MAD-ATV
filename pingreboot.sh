adc5f5d65964fb7957f8351911aedfa943a0d22f
Christian Betz <christian@betz.cc>
2020-08-17 14:21:04 +0200

    pingreboot rework for device flexibility

--- pingreboot.sh
+++ pingreboot.sh
@@ -1,19 +1,20 @@
 #!/system/bin/sh
-#version 2.0
+#version 2.1
 
-[ -f /sdcard/pingreboot ] || exit
-
-log -t ProtoWifi "pingreboot: started"
+log -t PingReboot "PingReboot: checking for /sdcard/pingreboot file"
+[ -f /sdcard/pingreboot ] || log -t PingReboot "/sdcard/pingreboot not existing, not starting PingReboot"; exit
+log -t PingReboot "PingReboot: started"
 
 # These values can be overridden by putting them
 # in $CONF_FILE with a custom value.
 # Simply copy & paste lines you want to change.
 #
-PING_HOST=1.1.1.1
+PING_HOST="google.com"
 RUN_EVERY=30
-REENABLE_EVERY=1
+REENABLE_EVERY=4
 REBOOT_AFTER=10
 
+DEVICE=$(ip route get 8.8.8.8 | sed -nr 's/.*dev ([^\ ]+).*/\1/p')
 source /sdcard/pingreboot
 
 c=0
@@ -22,14 +23,15 @@ while true; do
     c=0
   else
     c=$((c+1))
-    log -t ProtoWifi "pingreboot: network failure, could not ping $PING_HOST"
+    log -t PingReboot "PingReboot: network failure, could not ping $PING_HOST (c=$c)"
     if (( $c > $REBOOT_AFTER )); then
        reboot
     elif (( $c % $REENABLE_EVERY == 0 )); then
-      log -t ProtoWifi "pingreboot: re-enabling WiFi"
-      svc wifi disable
+      log -t PingReboot "PingReboot: re-enabling $DEVICE"
+      ifconfig down $DEVICE
       sleep 4
-      svc wifi enable
+      ifconfig up $DEVICE
+      log -t PingReboot "PingReboot: device $DEVICE re-enabled"
     fi
   fi
   sleep $RUN_EVERY
